c1d50989ce527277e6d5d2f4684528ebaa2996ab
Moatez-km <moatez.kamounn@gmail.com>
2026-09-07 18:35:11 +0200

    change backend port to 3001

--- apps/api/dist/main.js
+++ apps/api/dist/main.js
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    app.setGlobalPrefix('api');
+    app.enableCors({
+        origin: 'http://localhost:3001',
+        credentials: true,
+    });
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    await app.listen(3001);
 }
-await bootstrap();
+bootstrap();
 //# sourceMappingURL=main.js.map
