720e406f9b4b74e3a4f2d4a26360dbb81f21da63
Moatez-km <moatez.kamounn@gmail.com>
2026-09-24 13:29:21 +0200

    create card module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
 import { BoardsModule } from './boards/boards.module.js';
+import { CardsModule } from './cards/cards.module.js';
 
 @Module({
-  imports: [PrismaModule, AuthModule, BoardsModule],
+  imports: [PrismaModule, AuthModule, BoardsModule, CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
