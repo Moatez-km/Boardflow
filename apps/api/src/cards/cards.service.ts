3ab56a3330e4e6dd71255c2a7039ec93ed5a2d77
Moatez-km <moatez.kamounn@gmail.com>
2026-09-24 13:42:24 +0200

    add getcardwithboard methode

--- apps/api/src/cards/cards.service.ts
+++ apps/api/src/cards/cards.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+    ForbiddenException,
+    Injectable,
+    NotFoundException
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service.js';
+import { CreateCardDto } from './dto/create-card.dto.js';
+import { UpdateCardDto } from './dto/update-card.dto.js';
+import { ReorderCardDto } from './dto/reorder-card.dto.js';
+import { Prisma } from '@prisma/client';
+
 
 @Injectable()
-export class CardsService {}
+export class CardsService {
+    constructor(private prisma: PrismaService) { }
+    private async getCardWithBoard(cardId: string) {
+        const card = await this.prisma.card.findUnique({
+            where: { id: cardId },
+            include: {
+                board: true,
+                section: true,
+            },
+        });
+
+        if (!card) {
+            throw new NotFoundException('Card not found');
+        }
+
+        return card;
+    }
+
+
+
+}
